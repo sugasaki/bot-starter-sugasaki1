19fd37a6071efeda2f158b1d5ea16d6935a9686f
atsushi sugasaki <sugasaki@gmail.com>
2017-02-23 22:20:49 +0900

    composer.jsonビルドエラーに対応

--- index.php
+++ index.php
@@ -35,6 +35,10 @@ foreach ($events as $event) {
     continue;
   }
 
+
+error_log($event->getText());
+
+
 //  $message = "ありゃりゃ(happy)" . $event->getText();
  $message = "ありゃりゃ" . $emoji['kao1'];
    //$emoji = hex2bin( "F4809082F48087B3626F6F6BF48FBFBFF4809082F48087AD72756C6572F48FBFBFF4809082F48087B567696674F48FBFBF");
